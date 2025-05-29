b52185ebe70eb144cc641607f2781b3732eff15e
Dario Hamidi <dario.hamidi@gmail.com>
2025-05-29 11:52:10 +0300

    chore: remove unused import

--- step5.ts
+++ step5.ts
@@ -10,7 +10,6 @@ import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
 import type { CallToolRequest } from "@modelcontextprotocol/sdk/types.js";
 import { parseArgs } from "util";
-import { sys } from "typescript";
 
 const anthropic = new Anthropic();
 
